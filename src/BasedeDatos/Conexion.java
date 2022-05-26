fb1f00b6c1831427ce4f923303f12645475400be
Mayra Garrido <Mayra Garrido@192.168.1.73>
2022-05-26 08:33:08 -0500

    se suben los primeros abances del proyecto login y vistas principales

---
+++ src/BasedeDatos/Conexion.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BasedeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Mayra Garrido
+ */
+public class Conexion {
+    
+    
+    static String bd = "imagenes";
+    static String login = "root";
+    static String password = "";
+    static String url = "jdbc:mysql://localhost/"+bd;
+    Connection connection = null;
+
+    public Conexion(){
+        try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection =  DriverManager.getConnection(url,login,password);
+        if (connection!=null){
+            System.out.println("Conexión a base de datos "+bd+" OK\n");
+        }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+    public void desconectar(){
+        try {
+            connection.close();
+            System.out.println("cerrando coneccion");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    
+}
